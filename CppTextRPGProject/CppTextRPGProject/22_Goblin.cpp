5965a58ea9abf6919d5aa69cc441556d64cb51fd
DevCol <devcol.main@gmail.com>
2026-05-04 19:30:08 +0900

    All Complete

---
+++ CppTextRPGProject/CppTextRPGProject/22_Goblin.cpp
@@ -0,0 +1,15 @@
+﻿#include "22_Goblin.h"
+
+
+void Goblin::initializing()
+{
+    name = "Goblin";
+    hp = 10;
+    power = 10;
+    defence = 5;
+    dropItemName = "Goblin Leather";
+    dropItemPrice = 10;
+    
+  
+}
+
