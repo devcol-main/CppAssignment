4c06e2d3e53fd0fd1871eea483e13cd54d3102fd
DevCol <devcol.main@gmail.com>
2026-05-06 17:40:41 +0900

    init ex5

---
+++ CppTextRPGProject/EX5_InventoryTemplate/22_Goblin.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "20_Monster.h"
+
+class Goblin : public Monster
+{
+    
+private:
+    void initializing();
+ 
+public:
+
+
+    Goblin() 
+    {
+        initializing();       
+    }
+    
+
+};
+
+ 
